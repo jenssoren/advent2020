a0a6461e1b858f45e7663e07126dcbe74ddeb3fa
Jens Sørensen <jes@valt.dk>
2020-12-04 11:49:17 +0100

    Refactor day 4 to use new runner

--- 4/task1.js
+++ 4/task1.js
@@ -1,59 +1,39 @@
-const fs = require('fs').promises;
+const { Base } = require('../lib/base.js')
 
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
-}
-
-const filename = process.argv[2];
-
-async function run() {
-  filehandler = await load_input(filename);
-  data = handle(pass_input(filehandler));
-}
-
-run();
-
-function load_input(filename)
-{
-  return fs.readFile(filename, 'utf8');
-}
-
-function pass_input(data)
-{
-  let splited = data.split("\n");
-  let counter = 0;
-  let formatedData = [{}];
-  
-  for (let i = 0; i < splited.length; i++) {
-    if (splited[i] == "") {
-      formatedData.push({});
-      counter++;
-    } else {
-      params = splited[i].split(" ");
-      for (let j = 0; j < params.length; j++) {
-        [key, value] = params[j].split(":", 2);
-        formatedData[counter][key] = value;
+class Task extends Base {
+  pass_input(data) {
+    let splited = data.split("\n");
+    let counter = 0;
+    let formatedData = [{}];
+    
+    splited.forEach(row => {
+      if (row == "") {
+        formatedData.push({});
+        counter++;
+      } else {
+        row.split(" ").forEach(param => {
+          let key, value;
+          [key, value] = param.split(":", 2);
+          formatedData[counter][key] = value;
+        })
       }
-    }
+    })
+
+    return formatedData;
   }
 
-  return formatedData;
-}
+  handle(data) {
+    const validPassports = data.filter(passport => 
+      this.validate_keys(Object.getOwnPropertyNames(passport)
+    ))
 
-function handle(data)
-{
-  let validPassports = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (validate_keys(Object.getOwnPropertyNames(data[i]))) {
-      validPassports++;
-    }
+    console.log("Valid passports: ", validPassports.length);
   }
-
-  console.log("Valid passports: ", validPassports);
+  
+  validate_keys(keys) {
+    const required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    return required_keys.every(val => keys.indexOf(val) !== -1);
+  }
 }
 
-function validate_keys(keys) {
-  const required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-  return required_keys.every(val => keys.indexOf(val) !== -1);
-}
+exports.Task = Task;
--- 4/task2.js
+++ 4/task2.js
@@ -1,112 +1,94 @@
-const fs = require('fs').promises;
+const { Base } = require('../lib/base.js')
 
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
-}
+class Task extends Base {
+  pass_input(data) {
+    let splited = data.split("\n");
+    let counter = 0;
+    let formatedData = [{}];
+    
+    splited.forEach(row => {
+      if (row == "") {
+        formatedData.push({});
+        counter++;
+      } else {
+        row.split(" ").forEach(param => {
+          let key, value;
+          [key, value] = param.split(":", 2);
+          formatedData[counter][key] = value;
+        })
+      }
+    })
 
-const filename = process.argv[2];
+    return formatedData;
+  }
 
-async function run() {
-  filehandler = await load_input(filename);
-  data = handle(pass_input(filehandler));
-}
-
-run();
-
-function load_input(filename)
-{
-  return fs.readFile(filename, 'utf8');
-}
-
-function pass_input(data)
-{
-  let splited = data.split("\n");
-  let counter = 0;
-  let formatedData = [{}];
+  handle(data) {
+    const validPassports = data.filter(passport => this.validate(passport));
+    console.log("Valid passports: ", validPassports.length);
+  }
   
-  for (let i = 0; i < splited.length; i++) {
-    if (splited[i] == "") {
-      formatedData.push({});
-      counter++;
-    } else {
-      params = splited[i].split(" ");
-      for (let j = 0; j < params.length; j++) {
-        [key, value] = params[j].split(":", 2);
-        formatedData[counter][key] = value;
+  // NOTE TO SELF: THIS IS A TERRIBLE WAY TO VALIDATE!
+  // This method returns fails if the validator matches a value OUTSIDE the valid values OR true if no validator fails.
+  // This should be reversed so it only returns true if all validators have values INSIDE the valid values.
+  // Ex. I wrote data.iyr as data.iry. data.iry is undefined and therefore never gt 2020
+  // But I don't want to use time on rewriting this as this is just for fun and it returns the correct amount of valid passports.
+  validate(data) {
+    const keys = Object.getOwnPropertyNames(data);
+    const required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    if (!required_keys.every(val => keys.indexOf(val) !== -1)) {
+      return false;
+    }
+
+    if (data.byr < 1920 || data.byr > 2002) {
+      return false;
+    }
+
+    if (data.iyr < 2010 || data.iyr > 2020) {
+      return false;
+    }
+
+    if (data.eyr < 2020 || data.eyr > 2030) {
+      return false;
+    }
+
+    let hgt_unit = data.hgt.substr(data.hgt.length - 2);
+    let height = parseInt(data.hgt);
+
+    if (data.hgt != height.toString() + hgt_unit) {
+      return false;
+    }
+
+    if (!(hgt_unit == "cm" || hgt_unit == "in")) {
+      return false;
+    }
+
+    if (hgt_unit == "cm") {
+      if (height < 150 || height > 193) {
+        return false;
       }
     }
-  }
 
-  return formatedData;
-}
-
-function handle(data)
-{
-  let validPassports = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (validate_keys(data[i])) {
-      validPassports++;
+    if (hgt_unit == "in") {
+      if (height < 59 || height > 76) {
+        return false;
+      }
     }
-  }
 
-  console.log("Valid passports: ", validPassports);
-}
-
-function validate_keys(data) {
-  const keys = Object.getOwnPropertyNames(data);
-  const required_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-  if (!required_keys.every(val => keys.indexOf(val) !== -1)) {
-    return false;
-  }
-
-  if (data.byr < 1920 || data.byr > 2002) {
-    return false;
-  }
-
-  if (data.iyr < 2010 || data.iyr > 2020) {
-    return false;
-  }
-
-  if (data.eyr < 2020 || data.eyr > 2030) {
-    return false;
-  }
-
-  let hgt_unit = data.hgt.substr(data.hgt.length - 2);
-  let height = parseInt(data.hgt);
-
-  if (data.hgt != height.toString() + hgt_unit) {
-    return false;
-  }
-
-  if (!(hgt_unit == "cm" || hgt_unit == "in")) {
-    return false;
-  }
-
-  if (hgt_unit == "cm") {
-    if (height < 150 || height > 193) {
+    let hclReg = /#[0-9a-f]{6}$/g;
+    if (!hclReg.test(data.hcl)) {
       return false;
     }
-  }
 
-  if (hgt_unit == "in") {
-    if (height < 59 || height > 76) {
+    if (["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].indexOf(data.ecl) == -1) {
       return false;
     }
-  }
 
-  let hclReg = /#[0-9a-f]{6}$/g;
-  if (!hclReg.test(data.hcl)) {
-    return false;
-  }
+    if (data.pid.toString().length != 9) {
+      return false;
+    }
 
-  if (["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].indexOf(data.ecl) == -1) {
-    return false;
+    return true;
   }
-
-  if (data.pid.toString().length != 9) {
-    return false;
-  }
-
-  return true;
 }
+
+exports.Task = Task;
