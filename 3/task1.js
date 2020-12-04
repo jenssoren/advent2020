51188e052c39954787e6419d695c2f893f5fa7cf
Jens Sørensen <jes@valt.dk>
2020-12-04 11:34:29 +0100

    Refactor day 3 to new runner

--- 3/task1.js
+++ 3/task1.js
@@ -1,45 +1,26 @@
-const fs = require('fs').promises;
+const { Base } = require('../lib/base.js')
 
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
-}
-
-const filename = process.argv[2];
-
-async function run() {
-  filehandler = await load_input(filename);
-  data = handle(pass_input(filehandler));
-}
-
-run();
-
-function load_input(filename)
-{
-  return fs.readFile(filename, 'utf8');
-}
-
-function pass_input(data)
-{
-  let formatedData = data.split("\n");
-  
-  return formatedData.map(row => row.split(""));
-}
-
-function handle(data)
-{
-  let hitTrees = 0;
-  let col = 0;
-  for (var i = 1; i < data.length; i++) {
-    col = col + 3;
-    if (col > data[i].length - 1) {
-      col =  col - data[i].length;
-    }
-
-    if (data[i][col] == "#") {
-      hitTrees++;
-    }
+class Task extends Base {
+  pass_input(data) {
+    return data.split("\n").map(row => row.split(""));
   }
 
-  console.log("Hit trees: ", hitTrees);
+  handle(data) {
+    let hitTrees = 0,
+      col = 0;
+    for (var i = 1; i < data.length; i++) {
+      col = col + 3;
+      if (col > data[i].length - 1) {
+        col =  col - data[i].length;
+      }
+
+      if (data[i][col] == "#") {
+        hitTrees++;
+      }
+    }
+
+    console.log("Hit trees: ", hitTrees);
+  }
 }
+
+exports.Task = Task;
--- 3/task2.js
+++ 3/task2.js
@@ -1,58 +1,36 @@
-const { count } = require('console');
+const { Base } = require('../lib/base.js')
 
-const fs = require('fs').promises;
-
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
-}
-
-const filename = process.argv[2];
-
-async function run() {
-  filehandler = await load_input(filename);
-  data = handle(pass_input(filehandler));
-}
-
-run();
-
-function load_input(filename)
-{
-  return fs.readFile(filename, 'utf8');
-}
-
-function pass_input(data)
-{
-  let formatedData = data.split("\n");
-  
-  return formatedData.map(row => row.split(""));
-}
-
-function handle(data)
-{
-  let r1 = countHitTrees(data, 1, 1);
-  let r3 = countHitTrees(data, 3, 1);
-  let r5 = countHitTrees(data, 5, 1);
-  let r7 = countHitTrees(data, 7, 1);
-  let d2 = countHitTrees(data, 1, 2);
-
-  console.log("Hit trees: ", r1 * r3 * r5 * r7 * d2);
-}
-
-function countHitTrees(data, colSize, down)
-{
-  let hitTrees = 0;
-  let col = 0;
-  for (var i = down; i < data.length; i = i + down) {
-    col = col + colSize;
-    if (col > data[i].length - 1) {
-      col = col - data[i].length;
-    }
-
-    if (data[i][col] == "#") {
-      hitTrees++;
-    }
+class Task extends Base {
+  pass_input(data) {
+    return data.split("\n").map(row => row.split(""));
   }
 
-  return hitTrees;
+  handle(data) {
+    let r1 = this.countHitTrees(data, 1, 1);
+    let r3 = this.countHitTrees(data, 3, 1);
+    let r5 = this.countHitTrees(data, 5, 1);
+    let r7 = this.countHitTrees(data, 7, 1);
+    let d2 = this.countHitTrees(data, 1, 2);
+
+    console.log("Hit trees: ", r1 * r3 * r5 * r7 * d2);
+  }
+
+  countHitTrees(data, colSize, down) {
+    let hitTrees = 0;
+    let col = 0;
+    for (var i = down; i < data.length; i = i + down) {
+      col = col + colSize;
+      if (col > data[i].length - 1) {
+        col = col - data[i].length;
+      }
+
+      if (data[i][col] == "#") {
+        hitTrees++;
+      }
+    }
+
+    return hitTrees;
+  }
 }
+
+exports.Task = Task;
