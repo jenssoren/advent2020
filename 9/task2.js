2fd0975d839468a126dde1b4a7437f72644cdecf
Jens Sørensen <jes@valt.dk>
2020-12-09 09:40:15 +0100

    Even more refactor of task 2 of day 9

--- 9/task2.js
+++ 9/task2.js
@@ -3,37 +3,31 @@ const Task1 = require('./task1.js')
 class Task extends Task1.Task {
   handle (data) {
     const invalidNumber = this.findInvalidNumber(data)
-    const chain = this.findCorrectChain(data, invalidNumber).sort()
+    const correctChain = this.findCorrectChain(data, invalidNumber)
+    const first = correctChain.shift()
+    const last = correctChain.pop()
     console.log(
       'First number:',
-      data[0],
+      first,
       'Largest number:',
-      chain[chain.length - 1],
+      last,
       'Added:',
-      chain[0] + chain[chain.length - 1]
+      first + last
     )
   }
 
   findCorrectChain (data, invalidNumber) {
-    let correctChain = []
-
-    data.forEach((number, index) => {
-      const chain = this.findChain(data, invalidNumber, index)
-
-      if (chain.sum === invalidNumber && chain.chain.length >= 2) {
-        correctChain = chain.chain
-        return false
-      }
-    })
-
-    return correctChain
+    return data
+      .map((number, index) => this.findChain(data, invalidNumber, index))
+      .find(chain => chain.sum === invalidNumber && chain.chain.length >= 2)
+      .chain
+      .sort()
   }
 
   findChain (data, invalidNumber, start) {
     let sum = 0
     const chain = []
     let i = start
-
     while (sum < invalidNumber) {
       sum += data[i]
       chain.push(data[i])
